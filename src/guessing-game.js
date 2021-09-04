5a7cfc9770ec7838db4cbbc0d6a0267919d3f2cf
Alexander Mazalo <mralexmzs@gmail.com>
2021-09-04 14:01:33 +0300

    feat: task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.start = 0;
+        this.end = 0;
+    }
 
     setRange(min, max) {
-
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
-
+        return Math.round((this.start + this.end) / 2);
     }
 
     lower() {
-
+        this.end = this.guess();
     }
 
     greater() {
-
+        this.start = this.guess();
     }
 }
 
